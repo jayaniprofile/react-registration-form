60c57419899374719397267af29081f19730cd27
jayaniprofile <jayanichamodapathirana@gmail.com>
2023-06-11 14:32:14 +0530

    create registration form

---
+++ src/form/RegInput.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./RegInput.css";
+
+const RegInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className="RegInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default RegInput;
